71c05a888653db5f0c5e2274544b6fe9259052f5
Johan R <johan.geradeaus@gmail.com>
2025-03-23 19:30:07 +0100

    Add instructions on how to run

--- README.md
+++ README.md
@@ -2,4 +2,27 @@
 
 A small example on how you can combine a HTMX application with a charting library.
 
-Built with HTMX, Chart.js, and Bootstrap. Bun and Hono as backend.
+## Features
+
+- HTMX for dynamic updates
+- Chart.js for interactive charts
+- Bootstrap for styling
+- Bun and Hono as backend for efficient server-side processing
+
+## Install and run
+
+Install Bun (https://bun.sh/)
+
+Install dependencies
+
+`bun install`
+
+Start the server
+
+`bun run start`
+
+Open http://localhost:3000
+
+## Caveats
+
+It's just a simple example meant as inspiration, could very well be improved and optimized!
--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,59 @@
-import { Hono } from 'hono'
-import { serveStatic } from 'hono/bun'
-import { html, raw } from 'hono/html'
+import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+import { html, raw } from "hono/html";
 
-import { Card } from './components/Card'
+import { Card } from "./components/Card";
 
+const app = new Hono();
 
-const app = new Hono()
-
-app.get('/card', async (c) => {
-    return c.html(<Card />)
-})
+// The card
+app.get("/card", async (c) => {
+  return c.html(<Card />);
+});
 
 // The graph data
-app.get('/graphdata', async (c) => {
+app.get("/graphdata", async (c) => {
+  const numPoints = 6;
+  const data = {
+    label: "# of Votes",
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    values: Array.from(
+      { length: numPoints },
+      () => Math.floor(Math.random() * 15) + 5,
+    ),
+    type: "bar",
+    backgroundColor: [
+      "rgba(255, 99, 132, 0.2)",
+      "rgba(54, 162, 235, 0.2)",
+      "rgba(255, 206, 86, 0.2)",
+      "rgba(75, 192, 192, 0.2)",
+      "rgba(153, 102, 255, 0.2)",
+      "rgba(255, 159, 64, 0.2)",
+    ],
+    borderColor: [
+      "rgba(255, 99, 132, 1)",
+      "rgba(54, 162, 235, 1)",
+      "rgba(255, 206, 86, 1)",
+      "rgba(75, 192, 192, 1)",
+      "rgba(153, 102, 255, 1)",
+      "rgba(255, 159, 64, 1)",
+    ],
+  };
 
-    const numPoints = 6;
-    const data = {
-        "label": '# of Votes',
-        "labels": ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        "values": Array.from({ length: numPoints }, () => Math.floor(Math.random() * 15) + 5),
-        "type": 'bar',
-        "backgroundColor": [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-        ],
-        "borderColor": [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-        ]
-      };
+  const json = JSON.stringify(data);
 
-    const json = JSON.stringify(data)
-
-    return c.html(
-      html`<script>
-          renderGraph(${raw(json)});
-          </script>`
-    )
-})
+  return c.html(
+    html`<script>
+      renderGraph(${raw(json)});
+    </script>`,
+  );
+});
 
 // Serve static files
-app.get('/graph.js', serveStatic({ path: '/assets/graph.js' }))
-app.get('/', serveStatic({ path: '/assets/index.html' }))
+app.get("/graph.js", serveStatic({ path: "/assets/graph.js" }));
+app.get("/", serveStatic({ path: "/assets/index.html" }));
 
 export default {
   port: 3000,
   fetch: app.fetch,
-}
+};
